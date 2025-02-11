0f2fe847bf56ec88d5b05e20d475dd8a91de2939
yusufalmahroos <yousif4shukri@gmail.com>
2025-02-11 22:46:03 +0300

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path                   = require('path');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
